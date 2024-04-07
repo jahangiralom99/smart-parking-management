302759faeffca045cb141e3fc98ab5f338bf7507
jahangir <jahangiralomtuf99@gmail.com>
2024-04-07 21:05:52 +0600

    [UPDATE] -update error page

---
+++ src/app/error.js
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
+          <p className="mb-4 text-lg text-gray-600">
+            Oops! Looks like you're lost.
+          </p>
+          <div className="animate-bounce">
+            <svg
+              className="mx-auto h-16 w-16 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+              ></path>
+            </svg>
+          </div>
+          <p className="mt-4 text-gray-600">
+            Let's get you back{" "}
+            <a href="/" className="text-blue-500">
+              home
+            </a>
+            .
+          </p>
+          <button className="text-red-500"
+            onClick={
+              // Attempt to recover by trying to re-render the segment
+              () => reset()
+            }
+          >
+            Try again
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
